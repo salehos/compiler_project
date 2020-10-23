94b8350f144e7dac192eaa4e686f4e034dce60a1
salehos <salehos763@gmail.com>
2020-10-24 00:28:24 +0330

    First commit and First phase of project

---
+++ compilerProject.py
@@ -0,0 +1,36 @@
+import re
+from lark import Lark
+import sys, getopt
+from sys import argv
+
+Ids: dict = {"T_ID": "T_ID", "T_HEX": "T_INTLITERAL", "T_INT": "T_INTLITERAL", "T_STRINGLITERAL": "T_STRINGLITERAL",
+             "T_TRUE_BOOLEAN": "T_BOOLEANLITERAL", "T_CHARLITERAL": "T_CHARLITERAL",
+             "T_FALSE_BOOLEAN": "T_BOOLEANLITERAL", "T_DOUBLELITERAL": "T_DOUBLELITERAL"}
+
+
+def myparser(grammar, inputfile):
+    parser = Lark(grammar, parser="lalr", start='start')
+    data = parser.parse(inputfile)
+    output = ""
+    for token in data.children:
+        if token.type in Ids.keys():
+            output += Ids[token.type] + "    "
+        output += token + "\n"
+    print(output)
+    file = open("outputs/output.txt", "w+")
+    file.write(output)
+
+
+
+
+def main(argv):
+    f = open("testsANDscripts/tests/t05-integer-hex1.in", "r")
+    f2 = open("grammar.lark", "r")
+    inputfile = f.read()
+    grammar = f2.read()
+    myparser(grammar=grammar, inputfile=inputfile)
+    f.close()
+    f2.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
